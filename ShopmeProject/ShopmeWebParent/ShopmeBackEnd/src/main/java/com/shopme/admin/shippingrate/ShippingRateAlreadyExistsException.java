f79cf83ccd783bb9344137c0df44f73118d1372f
Kaneki377 <vietvo683@gmail.com>
2024-06-14 22:28:48 +0700

    ShippingRate Module

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.shopme.admin.shippingrate;
+
+public class ShippingRateAlreadyExistsException extends Exception {
+
+	public ShippingRateAlreadyExistsException(String message) {
+		super(message);
+	}
+
+	
+}
